7c288fc3e412e25bc3ae28b3662334a294cc5947
sierra-95 <michaelmachohi@gmail.com>
2024-11-26 00:18:46 +0300

    Comp vision v1

--- Computer_Vision/VC0706 Camera Module/Code/3_StoreImage.py
+++ Computer_Vision/VC0706 Camera Module/Code/3_StoreImage.py
@@ -1,21 +1,20 @@
 import serial
 
-# Configure the serial connection (adjust the port to match your Arduino's port)
-PORT = "/dev/ttyACM1"  # Replace with your port (e.g., COM3 on Windows, /dev/ttyUSB0 on Linux/Mac)
-BAUDRATE = 9600      # Match the baud rate in your Arduino sketch
-OUTPUT_FILE = "captured_image.jpg"  # Name of the file to save the image
+
+PORT = "/dev/ttyACM1"
+BAUDRATE = 9600 
+OUTPUT_FILE = "captured_image.jpg"  
 
 try:
     print("Opening serial connection...")
-    ser = serial.Serial(PORT, BAUDRATE, timeout=10)  # 10 seconds timeout
+    ser = serial.Serial(PORT, BAUDRATE, timeout=10)  
 
-    # Open a file to save the image
     with open(OUTPUT_FILE, "wb") as img_file:
         print(f"Waiting for image data from {PORT}...")
         while True:
             data = ser.read(32)  # Read in chunks of 32 bytes
             if not data:
-                break  # Exit when no more data is received
+                break 
             print(data[:10])
             img_file.write(data)
 
