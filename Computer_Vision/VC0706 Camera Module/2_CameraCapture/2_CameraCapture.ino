3be071bf790ceeaa81d331c12f86a94606685795
sierra-95 <michaelmachohi@gmail.com>
2024-11-25 23:54:37 +0300

    Comp vision v1

--- Computer_Vision/VC0706 Camera Module/2_CameraCapture/2_CameraCapture.ino
+++ Computer_Vision/VC0706 Camera Module/2_CameraCapture/2_CameraCapture.ino
@@ -1,43 +1,22 @@
 #include <Adafruit_VC0706.h>
 #define cameraconnection Serial1
 
-//Adafruit_VC0706 camera = Adafruit_VC0706(&Serial1); 
 Adafruit_VC0706 camera = Adafruit_VC0706(&cameraconnection);
 
 bool pictureTaken = false; 
 
 void setup() {
     Serial.begin(9600);     
-    //Serial1.begin(38400);
 
-    if (camera.begin()) {
-        Serial.println("Camera found!");
-    } else {
-        Serial.println("Camera not found. Check wiring!");
+    if (!camera.begin()) {
+        Serial.println("Camera not found!");
         return;
-    }
-    Camera();
+    }    
+    camera.setImageSize(VC0706_640x480);
     Capture();
 }
 
 void loop(){
-}
-
-void Camera(){
-  char *reply = camera.getVersion();
-  if (reply == 0) {
-    Serial.print("Failed to get version");
-  } else {
-    Serial.println("-----------------");
-    Serial.print(reply);
-  }
-
-  uint8_t imgsize = camera.getImageSize();
-  Serial.print("Image resolution: ");
-  if (imgsize == VC0706_640x480) Serial.println("640x480");
-  if (imgsize == VC0706_320x240) Serial.println("320x240");
-  if (imgsize == VC0706_160x120) Serial.println("160x120");
-  Serial.println("-----------------");
 }
 
 void Capture() {
@@ -66,7 +45,6 @@ void pyserial() {
           while (1);
       }
       Serial.write(buffer, bytesToRead);
-
       imgSize -= bytesToRead;
     }
 }
